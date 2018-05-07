a3b872d8e9af1cc9e26cae11cf9ffdbb72ee1861
Chirila Loredana <cute.niiko@gmail.com>
2018-05-07 16:19:03 -0500

    Add files via upload

--- gh-pages/upload.php
+++ gh-pages/upload.php
@@ -1,41 +1,40 @@
 <html>
 	<head>
-		<meta http-equiv = "refresh" content = "2; url = test.html">
+	<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
-		<div id="redirecting_content">
+		<div class="redirecting_content">
 			<?php
 				$target_dir = "uploads/";
-				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 				$uploadOk = 1;
-				$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-				if (isset($_POST["radio"])){
-					if($_POST["radio"] == "modify")
-						//do stuff
+				if (isset($_POST["fileToUpload"])){
+					
+					$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+					$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+					if($fileType != "json" || $fileType != "XML") {
+						echo ("<h2>Sorry, only JSON, XML files are allowed.</h2>");
+						die();
+					}
 					if (file_exists($target_file)) {
-						echo "<h2>Sorry, file already exists.</h2>";
-						$uploadOk = 0;
+						echo ("<h2>Sorry, file already exists.</h2>");
+						die();
 					}
 
 					if ($_FILES["fileToUpload"]["size"] > 500000) {
-						echo "<h2>Sorry, your file is too large.</h2>";
-						$uploadOk = 0;
+						echo ("<h2>Sorry, your file is too large.</h2>");
+						die();
 					}
 
-					if($fileType != "doc" && $fileType != "docx" && $fileType != "txt" && $fileType != "docm" && $fileType != "gdoc") {
-						echo "<h2>Sorry, only DOC, DOCX, DOCM, TXT and GDOC files are allowed.</h2>";
-						$uploadOk = 0;
-					}
-
-					if ($uploadOk == 0) {
-						echo "<h2>Sorry, your file was not uploaded.</h2>";
-					} else {
+					if ($uploadOk != 0) {
 						if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-							echo "<h2>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</h2>";
+							echo ("<h2>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</h2>");
+							die();
 						} else {
-							echo "<h2>Sorry, there was an error uploading your file.</h2>";
+							echo ("<h2>Sorry, there was an error uploading your file.</h2>");
+							die();
 						}
 					}
 				}

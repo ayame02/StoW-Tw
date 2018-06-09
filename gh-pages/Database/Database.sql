ed533b55082fd2fd244c1ed6471fbcc692c7cc47
Chirila Loredana <cute.niiko@gmail.com>
2018-06-09 04:15:52 -0500

    Update Database.sql

--- gh-pages/Database/Database.sql
+++ gh-pages/Database/Database.sql
@@ -15,7 +15,8 @@ CREATE TABLE story(
 	main_author VARCHAR(256),
 	secondary_authors JSON, 
 	characters JSON,
-	media_link JSON
+	media_link JSON,
+	story_image VARCHAR(256)
 );
 
 /
@@ -24,8 +25,9 @@ CREATE TABLE profile(
 	user_id INT(4) NOT NULL PRIMARY KEY,
 	general_pres TEXT,
 	family_members JSON,
-	favorite_stories JSON
+	favorite_stories INT
 );
+
 
 /
 
